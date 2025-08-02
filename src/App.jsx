8c193df2a994f309fc965361923ef91ad92a6c06
Sonu <sonu108rp@gmail.com>
2025-08-02 23:40:30 +0530

    Name style

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
 
   return (
     <>
-      <h1>SONU KUMAR</h1>
+    <div className='flex flex-col justify-center align-middle h-screen w-screen bg-[#0C0910] my-auto m-auto'>
+      <h1 className='text-white text-center text-[10vw] md:text-8xl'>SONU KUMAR</h1>
+    </div>
     </>
   )
 }
